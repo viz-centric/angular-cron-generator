6fc722d3fa3dc3679af1f2e80482dd2a1109d0b7
johib.khan@gmail.com <johib.khan@gmail.com>
2020-02-18 11:11:53 +0530

    custom and monthly issues are fixed

--- dist/angular-cron-generator.js
+++ dist/angular-cron-generator.js
@@ -127,12 +127,12 @@
         if (count >= 4) {
           cron[4] = typeof o.days !== "undefined" ? o.days : "*";
         }
-        if (count === 5) {
-          cron[4] = "*";
-        }
-        if (count > 5) {
+        if (count >= 5) {
           cron[4] = "*";
           cron[3] = typeof o.months !== "undefined" ? o.months : "*";
+        }
+        if (count === 5 || count === 7) {
+          cron[3] = "*";
         }
         return cron.join(" ");
       }
